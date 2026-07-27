a2d7b7e523dc8ef4fbc9e140f03e45966f25e142
qwertyu1opz <83985846+qwertyu1opz@users.noreply.github.com>
2026-07-27 18:17:24 +0300

    FIX

---
+++ config.h
@@ -0,0 +1,8 @@
+#ifndef LEGACYGRAM_CONFIG_H
+#define LEGACYGRAM_CONFIG_H
+
+// Insert your Telegram API credentials from https://my.telegram.org.
+#define SETUP_API_ID(apiId) apiId = 0;
+#define SETUP_API_HASH(apiHash) apiHash = @"";
+
+#endif
